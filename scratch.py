c7e1ee28cb43412f5c71a96e2c99576e9ed489d4
George Thayamkery <george.thayamkery@gmail.com>
2021-09-10 21:30:50 -0500

    Step 2: Here is how you can get the response and put into pandas

--- scratch.py
+++ scratch.py
@@ -1,5 +1,12 @@
 import requests
-import pandas
+import pandas as pd
 
 res = requests.get("https://pokeapi.co/api/v2/pokemon?limit=20=offset=0")
-print(res)
+
+if res.status_code == 200:
+    data = res.json()
+
+    pokemon_df = pd.DataFrame.from_dict(data["results"])
+    print(pokemon_df)
+else: 
+    print("server returned an error!")
